8fe0305e4eaea56b222c61ceea089749358fc833
Anne Homann <an.hmnn@gmail.com>
2019-07-06 10:20:43 +1000

    worked on url requests testing, made it fetch a 404 error

--- test_zodiac_zen.py
+++ test_zodiac_zen.py
@@ -1,12 +1,35 @@
-from main import greeting
-from main import get_readings
+"""
+Program: Zodiac Zen
+Author: Anne Homann
+Contact: css011906@coderacademy.edu.au
+Date: 2019/06/29
+Licence: GPLv3
+Version: 0.1
+Unit testing for Zodiac Zen using Pytest
+"""
+import requests
+from zodiac_zen import greeting
+from zodiac_zen import web_scrape
 
 def test_greeting(monkeypatch):
 
-    # monkeypatch the "input" function, so that it returns "Anne".
-    # This simulates the user entering "Anne" in the terminal:
-    monkeypatch.setattr('builtins.input', lambda x: "Anne")
+    """ monkeypatch the "input" function, so that it returns "Anne".
+    This simulates the user entering "Anne" in the terminal: """
 
+    monkeypatch.setattr('builtins.input', lambda x: "Anne")
     # go about using input() like you normally would:
     first = input("What is your first name?")
     assert first == "Anne"
+
+
+def test_web_scrape():
+
+    """ GET request to an incorrect URL and
+    return a 404. Display that an error would
+    occur as the sit cannot be reached """
+
+    url = 'http://annehomamm.github.io/astrology.html'
+    resp = requests.get(url)
+    assert resp.status_code == 404
+
+#def test_get_readings():
